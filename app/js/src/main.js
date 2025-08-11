cb7ec299a06f669e90ed6fecfe958908e7d23933
Иванов Владислав <ivanov.vladislav.yurevich@gmail.com>
2025-08-11 20:21:16 +0500

    add

--- app/js/src/main.js
+++ app/js/src/main.js
@@ -25,3 +25,81 @@ let mainAnalyticsPages = new Swiper(".main-analytics__pages", {
         swiper: mainAnalyticsBtns,
     },
 });
+
+//плавный скролл до якорей
+let offsets = {};
+
+function updateOffsets() {
+    if (window.innerWidth > 1200) {
+        offsets = {
+            'platform': 80,
+            'types': 120,
+            'analytics': -40,
+            'contact': 0
+        };
+    } else {
+        offsets = {
+            'platform': 80,
+            'types': 120,
+            'analytics': -40,
+            'contact': 0
+        };
+    }
+}
+
+// начальное определение
+updateOffsets();
+
+// пересчитываем при изменении размера окна
+let resizeTimeout;
+window.addEventListener('resize', () => {
+    clearTimeout(resizeTimeout);
+    resizeTimeout = setTimeout(updateOffsets, 150);
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        const targetID = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetID);
+
+        if (targetElement) {
+            e.preventDefault();
+
+            const elementPosition = targetElement.getBoundingClientRect().top + window.pageYOffset;
+            const offset = offsets[targetID] || 0;
+
+            window.scrollTo({
+                top: elementPosition - offset,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+let showFormBtn = document.querySelector('.footer__form'); // кнопка открыть
+let formFixed = document.querySelector('.form-fixed'); // фон
+let formContainer = document.querySelector('.form-fixed__container'); // сама форма
+
+// Открытие
+if (showFormBtn && formFixed) {
+    showFormBtn.addEventListener('click', function (e) {
+        e.preventDefault();
+        formFixed.classList.add('active');
+    });
+}
+
+// Закрытие при клике на фон
+if (formFixed && formContainer) {
+    formFixed.addEventListener('click', function (e) {
+        if (!formContainer.contains(e.target)) {
+            formFixed.classList.remove('active');
+        }
+    });
+}
+
+// Дополнительно: закрытие по Esc
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape') {
+        formFixed.classList.remove('active');
+    }
+});
